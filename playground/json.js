2c5ce5994ef4c81ec75fc8da26e42b4b3d0ca1d1
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-12-12 19:53:47 +0100

    playin with json

---
+++ playground/json.js
@@ -0,0 +1,12 @@
+// // let obj = {
+// //   name: 'Marcin'
+// // };
+// // let stringObj = JSON.stringify(obj);
+// // console.log(typeof stringObj);
+// // console.log(stringObj);
+// let personString = '{"name": "Marcin", "age": 24}';
+// let person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
