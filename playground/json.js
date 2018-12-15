0a2367bc880fdc73358d445f1c161735e6ea7b1b
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-12-15 12:04:54 +0100

    make new json file

--- playground/json.js
+++ playground/json.js
@@ -10,3 +10,13 @@
 // console.log(person);
 
 const fs = require('fs');
+let originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
