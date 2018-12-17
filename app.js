4fe4d7a0614a736294f0054ec68bcdc48242523a
marcinzelaskopl <marcinzelaskov@gmail.com>
2018-12-17 12:47:30 +0100

    redifining arguments

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js')
+let titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
 
-const argv = yargs.argv;
+let bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+}
+const argv = yargs
+.command('add', 'Add a new note', {
+  title: titleOptions,
+  body: bodyOptions
+})
+.command('list', 'List all notes')
+.command('read', 'Read a note', {
+  title: titleOptions
+})
+.command('remove', 'Remove a note', {
+  title: titleOptions
+})
+.help()
+.argv;
 let command = argv._[0];
 
 if (command === 'add') {
